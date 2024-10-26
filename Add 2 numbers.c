440f9404799d871b73cbfadef3defdcee0063bb8
ShrutiBaviskar <shrutibaviskar224@gmail.com>
2024-10-26 10:28:48 +0530

    Create Add 2 numbers.c

---
+++ Add 2 numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int a,b,c;
+    printf("add 2 numbers\n\n");
+    printf("Enter new a:");
+    scanf("%d",&a);
+    printf("Enter new b:");
+    scanf("%d",&b);
+    c=a+b;
+    printf("Result=%d",c);
+    printf("\n\n");
+    return 0;
+}
