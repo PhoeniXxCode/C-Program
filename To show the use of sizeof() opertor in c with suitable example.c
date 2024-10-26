0e2012595e5f18994c2973fd161584a8a5b1c940
ShrutiBaviskar <shrutibaviskar224@gmail.com>
2024-10-26 11:12:21 +0530

    Update and rename README.md to To show the use of sizeof() opertor in c with suitable example.c

---
+++ To show the use of sizeof() opertor in c with suitable example.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+    int a;
+    char b;
+    double c;
+    printf("Sizeof() operator in c\n\n");
+    printf("size of integer:%1u\n",sizeof(a));
+    printf("size of character:%1u\n",sizeof(b));
+    printf("size of double:%1u\n",sizeof(c));
+    printf("\n\n");
+    return 0;
+}
